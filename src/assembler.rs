34f0893b4c5edd5ded02f3a90cac03b338c0b051
aswarbs <amberswarbrick1@gmail.com>
2024-08-12 16:48:41 +0100

    fix negation and start assembler and rename method

--- src/assembler.rs
+++ src/assembler.rs
@@ -4,13 +4,13 @@ use crate::tokens::TAMInst::{ABS, ADD, DIV, MOD, MUL, NEG, SUB};
 pub fn ast_to_tam(ast: Expr) ->  Vec<TAMInst> {
     match ast {
         Expr::LitInteger(i) => { vec![TAMInst::LOADL(i)] },
-        Expr::BinOp(operator, left, right) => ast_to_tam(*left).into_iter().chain(ast_to_tam(*right)).chain(std::iter::once(get_tamOP_from_astOP(operator))).collect(),
-        Expr::UnOp(operator, value) => ast_to_tam(*value).into_iter().chain(std::iter::once(get_tamOP_from_astOP(operator))).collect(),
+        Expr::BinOp(operator, left, right) => ast_to_tam(*left).into_iter().chain(ast_to_tam(*right)).chain(std::iter::once(op_to_taminst(operator))).collect(),
+        Expr::UnOp(operator, value) => ast_to_tam(*value).into_iter().chain(std::iter::once(op_to_taminst(operator))).collect(),
     }
 
 }
 
-fn get_tamOP_from_astOP(operator: TokenOperator) -> TAMInst {
+fn op_to_taminst(operator: TokenOperator) -> TAMInst {
     match operator {
         TokenOperator::Plus => {ADD}
         TokenOperator::Minus => {SUB}
